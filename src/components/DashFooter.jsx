b2ce55fd11fe6f933efefff9fe62d3717ce9796e
itsbhav <118657839+itsbhav@users.noreply.github.com>
2025-08-17 03:30:57 +0530

    Update DashFooter.jsx

--- src/components/DashFooter.jsx
+++ src/components/DashFooter.jsx
@@ -36,20 +36,15 @@ const DashFooter = () => {
           }}
         >
           <div className="footer-div">
-            <Link
-              to={`mailto:${process.env.REACT_APP_OFFICIAL_MAIL}`}
-              style={{ color: "black" }}
-            >
-              Contact Us at {process.env.REACT_APP_OFFICIAL_MAIL}
-            </Link>
-          </div>
-          <div className="footer-div">
-            &copy; {year} | All Rights Reserved | eSkool
+            First Release {process.env.REACT_APP_FIRST_RELEASE}
           </div>
           <div className="footer-div">
             <Link to={"/instructions"}>
               <button className="button">instructions</button>
             </Link>
+          </div>
+          <div className="footer-div">
+            Latest Update {process.env.REACT_APP_LATEST_UPDATE}
           </div>
         </div>
       </div>
@@ -62,10 +57,15 @@ const DashFooter = () => {
           }}
         >
           <div className="footer-div">
-            First Release {process.env.REACT_APP_FIRST_RELEASE}
+            <Link
+              to={`mailto:${process.env.REACT_APP_OFFICIAL_MAIL}`}
+              style={{ color: "black" }}
+            >
+              Contact Us at {process.env.REACT_APP_OFFICIAL_MAIL}
+            </Link>
           </div>
           <div className="footer-div">
-            Latest Update {process.env.REACT_APP_LATEST_UPDATE}
+            &copy; {year} | All Rights Reserved | eSkool
           </div>
         </div>
       </div>
